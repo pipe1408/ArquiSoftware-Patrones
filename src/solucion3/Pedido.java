4d0847994dd8f3b1bfbff1ba38e055d4de7c9c3e
ValentinaRodRo <valentinarodro@unisabana.edu.co>
2024-08-08 21:57:20 -0500

    actualizacion clase Pedido

--- src/solucion3/Pedido.java
+++ src/solucion3/Pedido.java
@@ -1,32 +1,46 @@
 package solucion3;
 
-import java.util.List;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 abstract class Pedido {
     protected String idPedido;
     protected List<String> items;
+    protected double precioTotal;
+
+    public Pedido() {
+        this.idPedido = UUID.randomUUID().toString();
+        this.items = new ArrayList<>();
+        this.precioTotal = 0.0;
+    }
+
+    public void agregarItem(String item, double precio) {
+        items.add(item);
+        precioTotal += precio;
+    }
 
     public final void procesarPedido() {
         tomarPedido();
-        prepararPedido();
         verificarDisponibilidad();
+        prepararPedido();
         calcularPrecio();
         completarPedido();
     }
 
     protected void tomarPedido() {
-        System.out.println("Tomando el pedido...");
+        System.out.println("Tomando el pedido: " + idPedido);
+        System.out.println("Ítems del pedido: " + items);
     }
 
     protected abstract void prepararPedido();
     protected abstract void verificarDisponibilidad();
 
     protected void calcularPrecio() {
-        System.out.println("Calculando el precio total...");
+        System.out.println("Calculando el precio total: $" + precioTotal);
     }
 
     protected void completarPedido() {
-        System.out.println("Completando el pedido...");
+        System.out.println("Pedido " + idPedido + " completado.");
     }
 }
